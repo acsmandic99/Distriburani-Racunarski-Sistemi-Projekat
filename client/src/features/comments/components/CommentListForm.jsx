9e93436e401de39840d4484ab8ca81ca9c633555
DrOrange01 <20nikola01s@gmail.com>
2026-02-10 21:11:01 +0100

    odradjeni komentari

---
+++ client/src/features/comments/components/CommentListForm.jsx
@@ -0,0 +1,45 @@
+const CommentsList = ({ comments, currentUser, onDelete }) => {
+  if (comments.length === 0) {
+    return <p>No comments yet.</p>;
+  }
+
+  return (
+    <div>
+      {comments.map((comment) => (
+        <div key={comment._id} className="comment-item">
+          <div className="comment-header">
+            <strong>
+              {comment.comment_author.first_name}{" "}
+              {comment.comment_author.last_name}
+            </strong>
+            <span className="comment-date">
+              {new Date(comment.created_at).toLocaleString()}
+            </span>
+          </div>
+
+          <p className="comment-body">{comment.body}</p>
+
+          {comment.image_url && (
+            <img
+              src={comment.image_url}
+              alt="comment"
+              className="comment-image"
+            />
+          )}
+
+          {currentUser &&
+            currentUser._id === comment.comment_author.author_id && (
+              <button
+                className="comment-delete"
+                onClick={() => onDelete(comment._id)}
+              >
+                Delete
+              </button>
+            )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CommentsList;
---
+++ client/src/features/comments/services/commentAPI.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const getCommentsForRecipe = async (recipeId, skip = 0, limit = 10) => {
+  const res = await axios.get(`/comments/${recipeId}?skip=${skip}&limit=${limit}`);
+  return res.data.data;
+};
+
+export const addComment = async (formData) => {
+  const res = await axios.post("/comments/add-comment", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return res.data.data;
+};
+
+export const deleteComment = async (commentId) => {
+  const res = await axios.delete(`/comments/${commentId}`);
+  return res.data;
+};
