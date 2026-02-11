30e1afb983b7ceb49ba42f5603741d6c9bc97365
acsmandic99 <milos.mandicc@gmail.com>
2026-02-12 00:55:12 +0100

    test

--- client/src/features/admin/services/adminAPI.js
+++ client/src/features/admin/services/adminAPI.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import api from "../../shared/services/api/api";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+//const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = "https://tvoj-backend.onrender.com";
 const API_URL = `${BASE_URL}/api/v1/admin`;
 
 const getAuthHeaders = () => ({
