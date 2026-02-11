e0479049d16b274d217541766f8e236ba44f42e3
acsmandic99 <milos.mandicc@gmail.com>
2026-02-11 21:20:53 +0100

    tesrtttt

--- server/app/features/auth/services.py
+++ server/app/features/auth/services.py
@@ -31,7 +31,7 @@ class AuthService:
     @staticmethod
     def logout(jti):
         try:
-            redis_client.setex(f"blacklist:{jti}", 3600, "truee")
+            redis_client.setex(f"blacklist:{jti}", 3600, "true")
             return True
         except Exception as e:
             print(f"Redis logout error: {e}")
