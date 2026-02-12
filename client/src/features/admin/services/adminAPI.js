83840ed4f0abd7f63b5b9ddcbeea0896c7061435
acsmandic99 <milos.mandicc@gmail.com>
2026-02-12 01:05:01 +0100

    boze spasi

--- client/src/features/admin/services/adminAPI.js
+++ client/src/features/admin/services/adminAPI.js
@@ -2,9 +2,9 @@ import axios from "axios";
 import api from "../../shared/services/api/api";
 
 //const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const BASE_URL = "https://tvoj-backend.onrender.com";
+const BASE_URL = "https://backend-z574.onrender.com";
 //const API_URL = `${BASE_URL}/api/v1/admin`;
-const API_URL = "https://tvoj-backend.onrender.com/api/v1/admin";
+const API_URL = "https://backend-z574.onrender.com/api/v1/admin";
 
 const getAuthHeaders = () => ({
   headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
